115c070cc798019178905302c8aae695de37fc8d
leem00 <144612262+leem00@users.noreply.github.com>
2025-05-28 01:49:09 +0700

    Create telegram-iot.py

---
+++ telegram-iot.py
@@ -0,0 +1,61 @@
+import telepot
+from telepot.loop import MessageLoop
+import RPi.GPIO as GPIO
+from time import sleep 
+
+# dipercobaan ini saya pake led dan sensor inframerah. 
+# sesuaikan aja ya ges ya pin sama komponennya!
+led = 15
+cek = 14
+
+# variabel state dari sensor
+kondisi = ""
+
+# biasa lah ya
+GPIO.setmode(GPIO.BCM)      
+GPIO.setwarnings(False)
+GPIO.setup(led, GPIO.OUT) 
+GPIO.setup(cek, GPIO.IN) 
+
+# nah, ini kode buat handle input dari telegram
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+
+    # ini buat print command yang diterima telegram ke raspi
+    print ('Received:')
+    print(command)
+
+    # ini buat custom isi respon dari telegramnya mau diapain
+    # gak usah diajarin kan ya cara nyalain lampu sama baca sensor?
+    # lengkapnya pahamin sendiri aja ya!
+    if command == '/hi':
+        bot.sendMessage (chat_id, str("Hi! Ucim\nSaatnya mulai program dengan bismillah"))
+    elif command == '/bismillah':
+        bot.sendMessage (chat_id, str("Command Tersedia:\n/led_1 = menyalakan lampu\n" + 
+        "/led_0 = mematikan lampu\n" + "/state = cek sensor"))
+    elif command == '/led_1':
+        bot.sendMessage(chat_id, str("Red led is ON"))
+        GPIO.output(led, True)
+    elif command == '/led_0':
+        bot.sendMessage(chat_id, str("Red led is OFF"))
+        GPIO.output(led, False)
+    elif command == '/state':
+        if GPIO.input(cek) == 1:
+            kondisi = "Aman aja"
+        else:
+            kondisi = "Ada objek terdeteksi!"
+        bot.sendMessage(chat_id, str("Keadaan saat ini : {}").format(kondisi))
+    else:
+        bot.sendMessage(chat_id, str("Plisss deh, \nkeyword itu kagak ada di list perintah"))
+
+# Masukkin token telegram anda disini ya!
+bot = telepot.Bot('TOKEN BOT TELEGRAM ANDA')
+print (bot.getMe())
+
+# pokoknya ini mulai telegram
+MessageLoop(bot, handle).run_as_thread()
+print ('Listening....')
+
+while 1:
+    sleep(10)
